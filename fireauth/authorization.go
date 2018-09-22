5df527e99351203c8ac3a0d2183abccc681f610a
Zane Rockenbaugh <zane@liquid-labs.com>
2018-09-21 23:19:44 -0500

    moved and a little rearanging of the firebase stuff
    wrapped the client in a thin object to avoid passing context everywhere

---
+++ fireauth/authorization.go
@@ -0,0 +1,50 @@
+package fireauth
+
+import (
+  "fmt"
+  "strings"
+
+  "firebase.google.com/go/auth"
+
+  "github.com/Liquid-Labs/go-webapp-tools/rest"
+)
+
+func (ab *ScopedClient) CheckAuthorizedAll(reqClaims ...string) (*auth.Token, rest.RestError) {
+	token, err := ab.GetToken()
+	if err != nil {
+		return nil, err
+	}
+
+	claims := token.Claims
+	for _, reqClaim := range reqClaims {
+		claim, ok := claims[reqClaim]
+		if !ok || !claim.(bool) {
+			return nil, rest.AuthorizationError(fmt.Sprintf("User '%s' failed to access resource requiring claim '%s'.", token.UID, reqClaim), nil)
+		}
+	}
+
+	return token, nil
+}
+
+func (ab *ScopedClient) CheckAuthorizedAny(reqClaims ...string) (*auth.Token, rest.RestError) {
+	token, err := ab.GetToken()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(reqClaims) == 0 {
+		return token, nil
+	}
+
+	claims := token.Claims
+	for _, reqClaim := range reqClaims {
+		claim, ok := claims[reqClaim]
+		if ok && claim.(bool) {
+			return token, nil
+		}
+	}
+
+	return nil, rest.AuthorizationError(
+		fmt.Sprintf("User '%s' failed to access resource requiring at least one claim '%s'.",
+			 token.UID, strings.Join(reqClaims, ", ")), nil)
+}
