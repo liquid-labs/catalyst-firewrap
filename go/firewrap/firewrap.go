dd0983aaf43e83b4f1fe5ef179b8c46416bf6c0f
Zane Rockenbaugh <zane@liquid-labs.com>
2019-02-05 10:10:36 -0600

    get the firebase DB URL ourselves rather than have it passed in

--- go/firewrap/firewrap.go
+++ go/firewrap/firewrap.go
@@ -1,29 +1,29 @@
 package firewrap
 
 import (
-	"log"
 	"os"
 
 	"firebase.google.com/go"
 	"google.golang.org/api/option"
+	"github.com/Liquid-Labs/go-api/osext"
 )
 
 const credsKey string = "FIREBASE_CREDS_FILE"
+const dbUrlKey string = "FIREBASE_DB_URL"
+
+var firebaseDbUrl = osext.MustGetenv(dbUrlKey)
 var Config *firebase.Config
 var ClientOption option.ClientOption
 var Local bool = false
 
-func Setup(firebaseUrl string)  {
+func Setup()  {
 	Config = &firebase.Config{
-		DatabaseURL: firebaseUrl,
+		DatabaseURL: firebaseDbUrl,
 	}
 	// Fetch the service account key JSON file contents
 	var envPurpose = os.Getenv(`NODE_ENV`)
 	if envPurpose != "production" || Local {
-		localFirebaseCredsFile := os.Getenv(credsKey)
-		if localFirebaseCredsFile == "" {
-			log.Panicf("'%s' required for local operation.", credsKey)
-		}
+		localFirebaseCredsFile := osext.MustGetenv(credsKey)
 		ClientOption = option.WithCredentialsFile(localFirebaseCredsFile)
 	}
 }
